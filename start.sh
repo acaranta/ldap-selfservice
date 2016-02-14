5f48e514fdfc5a5e07d07e5eafcd19e79823de7d
Arthur Caranta <arthur@caranta.com>
2016-02-14 07:42:58 +0100

    Initial add of docker building

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ROOTTP="/var/www/ldap-selfservice"
+echo "Checking Teampass Files"
+if [ ! -f $ROOTTP/conf/config.inc.php ] ;
+then
+	echo "$ROOTTP/conf/config.inc.php not found creating it from template"
+	cp /app/config.inc.php.example $ROOTTP/conf/config.inc.php
+	chown -Rf www-data.www-data $ROOTTP 
+fi
+
+/usr/sbin/apache2ctl -D FOREGROUND &
+
+tail -f /var/log/apache2/*log
